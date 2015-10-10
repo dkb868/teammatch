c1e70ed528b7d93da0e0de3b29e176ea7d0944d6
Jeremy Neptune <jerenept@gmail.com>
2015-10-10 16:38:51 -0700

    added to admin

--- teammatch/hacker_matcher/admin.py
+++ teammatch/hacker_matcher/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from hacker_matcher.models import Hacker, Languages
+from hacker_matcher.models import * 
 
 admin.site.register(Hacker)
 admin.site.register(Languages)
+admin.site.register(Team)
+admin.site.register(Project)
+admin.site.register(Genres)
+admin.site.register(Platforms)
