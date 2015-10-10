b4e42e2e34f50f6c19c67d970dea23b915e1118f
Dmitri Brereton <mitrikyle@yahoo.com>
2015-10-10 14:27:26 -0700

    added profile setup

--- teammatch/hacker_matcher/urls.py
+++ teammatch/hacker_matcher/urls.py
@@ -4,5 +4,6 @@ from django.conf.urls import patterns, url
 import views
 
 urlpatterns = patterns('',
-        url(r'^$', views.index, name='index'))
+        url(r'^$', views.index, name='index'),
+        url(r'^profile_setup/', views.profle_setup, name='profile_setup'),)
 
--- teammatch/hacker_matcher/views.py
+++ teammatch/hacker_matcher/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 def index(request):
     context_dict = {'nothing': "nothing"}
     return render(request, 'hacker_matcher/index.html', context_dict)
+
+def profle_setup(request):
+    return render(request, 'hacker_matcher/profile_setup.html')
