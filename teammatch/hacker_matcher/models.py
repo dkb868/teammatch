07abdb7978b4f2dfcbbf03afd5392b67aabd36b2
Jeremy Neptune <jerenept@gmail.com>
2015-10-10 12:00:44 -0700

    fixed models

--- teammatch/hacker_matcher/models.py
+++ teammatch/hacker_matcher/models.py
@@ -1,18 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class Hacker(models.Model):
-	name = models.CharField()
-	languages_pro = models.ManyToManyField(Languages, blank = True)
-	languages_noob = models.ManyToManyField(Languages, blank = True)
-	team = models.ForeignKey(Team, blank = True)
-	slack = models.CharField()
-	is_competitive = models.BooleanField()
-	education_school = models.CharField(max_length = 2, choices = school_options)
-	education_year = models.CharField(max_length = 2, choices = year_in_school_options)
-	project_genre_wanted = models.ForeignKey(Genres, blank = True)
-	
-
 school_options = (
 	('HS', 'High School'),
 	('CL', 'College'),
@@ -24,26 +12,39 @@ year_in_school_options = (
 	("JR", "Junior"),
 	("SR", 'Senior')
 )
+class Hacker(models.Model):
+	name = models.CharField(max_length = 150)
+	languages_pro = models.ManyToManyField('Languages', blank = True, related_name = 'hacker_languages_pro')
+	languages_noob = models.ManyToManyField('Languages', blank = True, related_name = 'hacker_languages_noob')
+	team = models.ForeignKey('Team', blank = True)
+	slack = models.CharField(max_length = 50)
+	is_competitive = models.BooleanField()
+	education_school = models.CharField(max_length = 2, choices = school_options)
+	education_year = models.CharField(max_length = 2, choices = year_in_school_options)
+	project_genre_wanted = models.ForeignKey('Genres', blank = True)
+	
+
+
 	
 class Languages(models.Model):
-	name = models.CharField()
+	name = models.CharField(max_length = 50)
 
 	
 class Team(models.Model):
-	name = models.CharField()
-	languages = models.ManyToManyField(Languages)
-	project = models.ForeignKey(Project)
-	team_owner = models.ForeignKey(Hacker)
-	languages_wanted = models.ManyToManyField(Languages)
+	name = models.CharField(max_length = 150)
+	languages = models.ManyToManyField('Languages', related_name = 'team_languages')
+	project = models.ForeignKey('Project')
+	team_owner = models.ForeignKey('Hacker', related_name = 'team_owner')
+	languages_wanted = models.ManyToManyField('Languages', related_name = 'team_languages_wanted')
 	
 class Project(models.Model):
-	name = models.CharField()
-	genre = models.ForeignKey(Genres)
-	platform = models.ManyToManyField(Platforms)
+	name = models.CharField(max_length = 150)
+	genre = models.ForeignKey('Genres')
+	platform = models.ManyToManyField('Platforms')
 
 class Genres(models.Model):
-	name = models.CharField()
+	name = models.CharField(max_length = 50)
 	 
 class Platforms(models.Model):
-	name = models.CharField()
-	platform_type = models.CharField()
+	name = models.CharField(max_length = 50)
+	platform_type = models.CharField(max_length = 50)
