7f14bdbcbfe8aef9275527f77172292d40ffdb27
Dmitri Brereton <mitrikyle@yahoo.com>
2015-10-10 19:52:54 -0700

    lol

--- teammatch/hacker_matcher/views.py
+++ teammatch/hacker_matcher/views.py
@@ -6,9 +6,6 @@ def index(request):
     context_dict = {'nothing': "nothing"}
     return render(request, 'hacker_matcher/index.html', context_dict)
 
-def profle_setup(request):
-    return render(request, 'hacker_matcher/profile_setup.html')
-
 def matches(request):
     return render(request, 'hacker_matcher/matches.html')
 
@@ -25,6 +22,7 @@ def profile_setup(request):
             hacker = form.save(commit=False)
             hacker.user = request.user
             hacker.save()
+            form.save_m2m()
 
             return index(request)
         else:
