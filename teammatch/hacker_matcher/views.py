2f66a4ca5a7f62603ad2e092b4f6b35cd276221d
mitrikyle <mitrikyle@yahoo.com>
2015-10-17 22:03:22 -0700

    Fixed querysets to only get teams where compatability hasn't already been calculated # TODO algorithm in view

--- teammatch/hacker_matcher/views.py
+++ teammatch/hacker_matcher/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from hacker_matcher.forms import HackerForm
-import hacker_matcher.models
-
+from hacker_matcher.models import Team, Hacker, Compatability
 
 
 
@@ -12,6 +11,30 @@ def index(request):
     return render(request, 'hacker_matcher/index.html', context_dict)
 
 def matches(request):
+    # Testing the general idea of the algorithm
+    # Get teams and hacker
+
+    current_hacker = Hacker.objects.get(user=request.user)
+
+    # Get all the compatabilities that the user already has
+    compatability_already_done = Compatability.objects.filter(hacker = current_hacker)
+    # get all teams whose compatability is in the above variable,
+    # i.e who have a compatability with the current hacker already
+    team_already_done = Team.objects.filter(compatability__in=compatability_already_done)
+    print team_already_done
+    # get list of teams that user doesn't already have compatability with
+    teams = Team.objects.exclude(id__in=team_already_done)
+    print teams
+    # Compare hacker to every team and create a compatability object for this relation
+    for x in teams:
+
+        # ALGORITHM GOES HERE
+        score = 3 # ALGORITHM RESULT GOES HERE
+        # Create the new compatability with the score from the algortihm.
+        # This only affects teams that it has not alraedy been calculated for
+        compatability = Compatability.objects.create(hacker = current_hacker, team = x, value = score )
+        compatability.save()
+
     return render(request, 'hacker_matcher/matches.html')
 
 def signup(request):
