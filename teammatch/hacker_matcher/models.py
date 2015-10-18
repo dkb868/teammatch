f91f70827ba35366d4111da73f5dc5ecb343b5a7
mitrikyle <mitrikyle@yahoo.com>
2015-10-17 22:42:24 -0700

    Unified Hacker and Team models deleted projects mdoel, added experience

--- teammatch/hacker_matcher/models.py
+++ teammatch/hacker_matcher/models.py
@@ -1,65 +1,48 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-school_options = (
-    ('HS', 'High School'),
-    ('CL', 'College'),
-    ('GS', 'Graduate Student'),
-    ('GR', 'Graduated'),
-)
-year_in_school_options = (
-    ('FR', "Freshman"),
-    ("SO", "Sophomore"),
-    ("JR", "Junior"),
-    ("SR", 'Senior'),
-)
-
+# User Profile
 class Hacker(models.Model):
     user = models.OneToOneField(User, blank=True, null=True)
     name = models.CharField(max_length = 150, blank = True, null=True)
     languages_pro = models.ManyToManyField('Languages', blank = True, related_name = 'hacker_languages_pro', null=True)
     languages_noob = models.ManyToManyField('Languages', blank = True, related_name = 'hacker_languages_noob', null=True)
-    slack = models.CharField(max_length = 50, blank = True, null=True)
     is_competitive = models.BooleanField(blank=True, default=True)
-    education_school = models.CharField(max_length = 2, choices = school_options, blank = True, null=True)
-    education_year = models.CharField(max_length = 2, choices = year_in_school_options, blank = True, null=True)
-    school_name = models.CharField(max_length = 150, blank = True, null=True)
-    project_genre_wanted = models.ForeignKey('Genres', blank = True, null = True)
-    platforms_wanted = models.ForeignKey('Platforms', related_name = 'hacker_platforms_wanted',blank = True, null = True)
+    genres = models.ManyToManyField('Genres', blank = True, null = True)
+    platforms = models.ManyToManyField('Platforms', related_name = 'hacker_platforms_wanted',blank = True, null = True)
+    experience = models.ForeignKey('Experience',blank=True,null=True)
+    # Many to many field used to store compatability between hacker and teams
     teams = models.ManyToManyField('Team', through='Compatability', null=True)
     
     def __unicode__(self):
         return self.name
 
-class Languages(models.Model):
-    name = models.CharField(max_length = 50)
-    
+class Team(models.Model):
+    team_owner = models.ForeignKey('Hacker',blank = True, null = True)
+    name = models.CharField(max_length = 150,blank = True, null = True)
+    languages = models.ManyToManyField('Languages', related_name = 'team_languages_wanted',blank = True, null = True)
+    is_competitive = models.BooleanField(blank=True, default=True)
+    genres = models.ManyToManyField('Genres', blank = True, null = True)
+    platforms = models.ManyToManyField('Platforms', related_name = 'team_platforms_wanted',blank = True, null = True)
+    experience = models.ManyToManyField('Experience',blank=True,null=True)
+
     def __unicode__(self):
         return self.name
 
-class Team(models.Model):
-    name = models.CharField(max_length = 150,blank = True, null = True)
-    languages = models.ManyToManyField('Languages', related_name = 'team_languages',blank = True, null = True)
-    project = models.ForeignKey('Project',blank = True, null = True)
-    team_owner = models.ForeignKey('Hacker',blank = True, null = True)
-    languages_wanted = models.ManyToManyField('Languages', related_name = 'team_languages_wanted',blank = True, null = True)
-    
-    def __unicode__(self):
-        return self.name 
-           
-class Project(models.Model):
-    name = models.CharField(max_length = 150)
-    genre = models.ForeignKey('Genres')
-    platform = models.ManyToManyField('Platforms')
-    
+class Experience(models.Model):
+    name = models.CharField(max_length=50)
+
     def __unicode__(self):
         return self.name
+
+class Languages(models.Model):
+    name = models.CharField(max_length = 50)
 
 class Genres(models.Model):
     name = models.CharField(max_length = 50)
     def __unicode__(self):
         return self.name
+
 class Platforms(models.Model):
     name = models.CharField(max_length = 50)
     platform_type = models.CharField(max_length = 50, blank=True, null=True)
