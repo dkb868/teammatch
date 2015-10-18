5e58941da495bc78349a9d0b72deba78414f117d
mitrikyle <mitrikyle@yahoo.com>
2015-10-17 22:41:45 -0700

    got rid of projects model

--- teammatch/hacker_matcher/admin.py
+++ teammatch/hacker_matcher/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
-from hacker_matcher.models import Hacker, Languages, Team, Project, Genres, Platforms, Compatability
+from hacker_matcher.models import Hacker, Languages, Team, Genres, Platforms, Compatability
 
 admin.site.register(Hacker)
 admin.site.register(Languages)
 admin.site.register(Team)
-admin.site.register(Project)
 admin.site.register(Genres)
 admin.site.register(Platforms)
 admin.site.register(Compatability)
