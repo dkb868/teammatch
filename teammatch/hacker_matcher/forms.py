d149343bb7232131476412a8293a2775d133bb01
mitrikyle <mitrikyle@yahoo.com>
2015-10-18 16:33:39 -0700

    added team creation form

--- teammatch/hacker_matcher/forms.py
+++ teammatch/hacker_matcher/forms.py
@@ -1,18 +1,16 @@
 __author__ = 'mitri'
 
 from django import forms
-from hacker_matcher.models import Hacker
+from hacker_matcher.models import Hacker, Team
 
-dhoices = (
-    ('1', 'Java'),
-    ('2', 'Python'),
-    ('3', 'Ruby'),
-    ('4', 'Swift'),
-)
 
 class HackerForm(forms.ModelForm):
 
-    languages_pro = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, choices=dhoices)
     class Meta:
         model = Hacker
-        exclude = ('',)
+        exclude = ('teams',)
+
+class TeamForm(forms.ModelForm):
+    class Meta:
+        model = Team
+        exclude = ('team_owner',)
